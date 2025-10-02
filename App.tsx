1223691ad8cded7916d182e7a0d4993690db3304
Awongiwe <st10478718@vcconnect.edu.za>
2025-10-02 11:51:22 +0200

    Initial commit - Expo app setup

---
+++ App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { StyleSheet, TextInput, View, Alert, Button } from 'react-native';
+
+export default function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlePress = () => {
+    Alert.alert("Email Entered", email);
+  };
+  return (
+    <View style={styles.container}>
+  <TextInput 
+  style={styles.input}
+  placeholder="Enter your email"
+  value={email}
+  onChangeText={setEmail}
+  keyboardType="email-address"
+  autoCapitalize="none"
+/>
+<TextInput 
+  style={styles.input}
+  placeholder="Enter your password"
+  value={password}
+  onChangeText={setPassword}
+  secureTextEntry
+/>
+  <Button title="Submit" onPress={handlePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+});
